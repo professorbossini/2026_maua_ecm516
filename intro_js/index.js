39c13a21cc887e83b2be61491f8081743e558f15
Rodrigo Bossini <professorbossini@gmail.com>
2026-02-28 08:41:22 -0300

    first commit

---
+++ intro_js/index.js
@@ -0,0 +1,89 @@
+
+
+
+//operadores de comparação por igualdade: == e ===
+//null e undefined
+// console.log([] == [])
+// console.log([] == false)
+// console.log(null == undefined)
+// console.log(null == null)
+// console.log(1 == [1])
+// console.log(true == 1)
+// console.log(1 === 1)
+// console.log(1 === '1')
+// console.log(1 == '1')
+// console.log(1 === 1)
+// console.log(1 == 1)
+
+//coerção implícita e explícita
+// const n1 = 2
+// const n2 = '3'
+// const n3 = n1 + n2
+// console.log(n3)
+// const n4 = n1 + Number(n2)
+// console.log(n4)
+
+
+// matriz = [[], []]
+
+// List  <List <String > > matriz = new ArrayList <ArrayList <String>>();
+
+// a = 2
+
+
+
+// a.concat("b")
+// console.log(a)
+// console.log(typeof(a))
+// String a = "abc";
+// console.log(a)
+// console.log(typeof(a))
+//e o var?
+//não use
+//içamento (hoisting)
+// var idade = 18
+// console.log(`Oi, ${nome}`)
+// if(idade >= 18){
+//   let nome = 'João'
+//   console.log("Parabéns, " + nome + ". Você pode dirigir")
+// }
+// console.log('Até, ' + nome)
+// var linguagem = 'Javascript'
+// console.log('Aprendendo ' + linguagem)
+// var linguagem = 'Java'
+// console.log(`Aprendendo ${linguagem}`)
+
+
+// var c = 2
+// c = 3
+// console.log(c)
+//declarando variáveis
+// let a = 2
+// console.log(a)
+// let passouDeAno = true
+// console.log(passouDeAno)
+// a = 3
+// console.log(a)
+// let b
+// console.log(b)
+// b = 1.5
+// console.log(typeof(b))
+// console.log(b)
+// b = "abc"
+// console.log(typeof(b))
+// console.log(b)
+
+
+//declarando constantes
+//em Javascript
+// const nome = 'José'
+// console.log(nome)
+// // nome = 'José Silva'
+// const idade = 17
+// console.log(typeof(idade))
+
+//se fosse Java...
+// String nome = "José";
+//nome = 1;
+
+// console.log('Hello, World')
