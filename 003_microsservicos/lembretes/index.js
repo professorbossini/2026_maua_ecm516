1bf61b09fad621b5d5b1841213b82b0050910df3
Rodrigo Bossini <professorbossini@gmail.com>
2026-09-12 10:25:19 -0300

    learn(ap01-mss): encerra secao 4.3.70
    - retomar criando servico para mss de lembretes

--- 003_microsservicos/lembretes/index.js
+++ 003_microsservicos/lembretes/index.js
@@ -28,5 +28,7 @@ app.post('/eventos', (req, res) => {
 })
 
 app.listen(4000, () => {
+    console.log('Nova versão')
+    console.log('Agora usando o Docker Hub')
     console.log('Lembretes. Porta 4000.')
 })
